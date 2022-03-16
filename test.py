06463992e8ffd42a2341bb06199ea67b9aac4ae5
chandra15 <chandra.spdev15@gmail.com>
2022-03-16 17:46:45 +0530

    test4

--- test.py
+++ test.py
@@ -14,15 +14,17 @@ def change_protected_branch_settings():
     # Loops through all the repos and changes the /settings/branch_protection_rules/
     # CAREFUL with this!
     for repo in g.get_user().get_repos():
-        branch = repo.get_branch("gh-pages")
+        branch = repo.get_branch("main")
         branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
         print("Edited the branch protection rules for: " + repo.name)
 
 def change_protected_branch_settings_test():
-    repo = g.get_repo("philips-internal/synergy-gh-pages")
-    branch = g.get_repo("philips-internal/synergy-gh-pages").get_branch("gh-pages")
+    repo = g.get_repo("Chandrasekhar674/secure-ghpages")
+    branch = g.get_repo("Chandrasekhar674/secure-ghpages").get_branch("main")
     branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
 
 # Uncomment out one of these lines. Test to test a single repo/branch.
 #change_protected_branch_settings()
 #change_protected_branch_settings_test()
+
+print('Unable to load values from environment')
