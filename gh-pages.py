87143fb1713bc844ec3038b7166c676610c65b36
chandra15 <chandra.spdev15@gmail.com>
2022-03-16 16:54:20 +0530

    test

--- gh-pages.py
+++ gh-pages.py
@@ -7,12 +7,12 @@ def change_protected_branch_settings():
     # Loops through all the repos and changes the /settings/branch_protection_rules/
     # CAREFUL with this!
     for repo in g.get_user().get_repos():
-        branch = repo.get_branch("dev1")
+        branch = repo.get_branch("main")
         branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
         print("Edited the branch protection rules for: " + repo.name)
 
 def change_protected_branch_settings_test():
     repo = g.get_repo("Chandrasekhar674/secure-ghpages")
-    branch = g.get_repo("Chandrasekhar674/secure-ghpages").get_branch("dev1")
+    branch = g.get_repo("Chandrasekhar674/secure-ghpages").get_branch("main")
     branch.edit_protection(required_approving_review_count=2, enforce_admins=True)
 
